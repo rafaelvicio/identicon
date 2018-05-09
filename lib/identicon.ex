6b555666e8c7b3b588b45b88bd089eae39c08ac2
Rafael Vicio <rafaelvicio@icloud.com>
2018-05-08 23:16:10 -0300

    Create Imagem

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -5,6 +5,41 @@ defmodule Identicon do
     |> pick_color
     |> build_grid
     |> filter_odd_squares
+    |> build_pixel_map
+    |> draw_image
+    |> save_image(input)
+  end
+
+  def save_image(image, input) do
+    File.write("#{input}.png", image)
+  end
+
+  def draw_image(image) do
+    %Identicon.Image{pixel_map: map, color: color} = image
+    image = :egd.create(250, 250)
+        fill = :egd.color(color)
+
+        Enum.each map, fn({start, stop}) ->
+            :egd.filledRectangle(image, start, stop, fill)
+        end
+
+    :egd.render(image)
+  end
+
+  def build_pixel_map(image) do
+    %Identicon.Image{grid: grid} = image
+
+    pixel_map = Enum.map grid, fn({_code, index}) ->
+      horizontal = rem(index, 5) * 50
+      vertical = div(index, 5) * 50
+
+      top_left = {horizontal, vertical}
+      bottom_rigth = {horizontal + 50, vertical + 50}
+
+      {top_left, bottom_rigth}
+    end
+
+    %Identicon.Image{image | pixel_map: pixel_map}
   end
 
   def filter_odd_squares(image) do
