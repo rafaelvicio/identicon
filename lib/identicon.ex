e74658fd5bf9f225c83b800a797d43b90b91facf
Rafael Vicio <rafaelvicio@icloud.com>
2018-05-08 22:53:20 -0300

    filter odd squares

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -4,14 +4,29 @@ defmodule Identicon do
     |> hash_input
     |> pick_color
     |> build_grid
+    |> filter_odd_squares
+  end
+
+  def filter_odd_squares(image) do
+    %Identicon.Image{grid: grid} = image
+    grid = Enum.filter grid, fn({code, _index}) ->
+      rem(code, 2) == 0
+    end
+
+    %Identicon.Image{image | grid: grid}
   end
 
   def build_grid(image) do
     %Identicon.Image{hex: hex} = image
-    hex
-    |> Enum.chunk(3)
-    |> mirror_row
-    |> Enum.map(&mirror_row/1)
+    grid =
+      hex
+      |> Enum.chunk(3)
+      |> mirror_row
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
   end
 
   def mirror_row(row) do
